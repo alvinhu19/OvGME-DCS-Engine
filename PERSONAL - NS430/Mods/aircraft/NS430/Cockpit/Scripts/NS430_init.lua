d280a03aa67d3a4119f245b14d247f20ab6afcb8
Vaclav Danek <danvac@seznam.cz>
2019-01-18 16:41:27 +0100

    NS430: added viewport for export

--- PERSONAL - NS430/Mods/aircraft/NS430/Cockpit/Scripts/NS430_init.lua
+++ PERSONAL - NS430/Mods/aircraft/NS430/Cockpit/Scripts/NS430_init.lua
@@ -3,6 +3,11 @@ dofile(LockOn_Options.common_script_path.."devices_defs.lua")
 indicator_type = indicator_types.COMMON
 purposes = {render_purpose.GENERAL}--,render_purpose.HUD_ONLY_VIEW}  -- HUD will be rendered on hud only view 
 
+--- EDIT: add viewport ---
+dofile(LockOn_Options.common_script_path.."ViewportHandling.lua")
+try_find_assigned_viewport("NS430")
+--- EDIT: add viewport ---
+
 local my_path = LockOn_Options.script_path
 
 page_subsets = {
