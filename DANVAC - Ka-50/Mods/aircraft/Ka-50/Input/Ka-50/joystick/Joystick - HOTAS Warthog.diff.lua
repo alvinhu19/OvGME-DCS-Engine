b7b6176e3a1b2331364a4963e14f32e298ff2916
Vaclav Danek <danvac@seznam.cz>
2019-11-16 09:35:04 +0100

    Ka-50: initial diff files commit

---
+++ DANVAC - Ka-50/Mods/aircraft/Ka-50/Input/Ka-50/joystick/Joystick - HOTAS Warthog.diff.lua
@@ -0,0 +1,306 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.15,
+						},
+						["deadzone"] = 0.03,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "Flight Control Cyclic Pitch",
+		},
+		["a2002cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.15,
+						},
+						["deadzone"] = 0.03,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Flight Control Cyclic Roll",
+		},
+	},
+	["keyDiffs"] = {
+		["d103pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Shkval Narrow View 23x",
+		},
+		["d104pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Shkval Wide View 7x",
+		},
+		["d1589pnilu1590cdnilvdnilvpnilvunil"] = {
+			["name"] = "SPU-9 PTT",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+		},
+		["d350pnilu351cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Release weapons",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+		},
+		["d412pnilu414cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "TV Target frame Increase size",
+		},
+		["d413pnilu414cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "TV Target frame Decrease size",
+		},
+		["d506pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+			["name"] = "Engage/Disengage Route Mode",
+		},
+		["d507pnilu508cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+			["name"] = "Engage Descent Mode",
+		},
+		["d537pnilu385cdnilvdnilvpnilvunil"] = {
+			["name"] = "Hover On/Off",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+		},
+		["d538pnilu297cdnilvdnilvpnilvunil"] = {
+			["name"] = "Autopilot Emergency Off",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+		},
+		["d824pnilu381cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Weapon selection inward stations",
+		},
+		["d825pnilu381cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Weapon selection outward stations",
+		},
+		["d826pnilu382cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Weapon selection all stations",
+		},
+		["d827pnilu382cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "Weapon selection A-A mode",
+		},
+		["d833pnilu834cdnilvdnilvpnilvunil"] = {
+			["name"] = "Cockpit gauges illumination (stick)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+		},
+		["d92pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "KU-31 Shkval center",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+		},
+		["d957pnilu958cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+			["name"] = "Trimmer",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+				},
+			},
+		},
+		["d97pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+			["name"] = "Trimmer reset",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UR",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+		["dnilp439u440cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Uncage SHKVAL, designate target",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+		},
+		["dnilp88u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "KU-31 Shkval slew left",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+		},
+		["dnilp89u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "KU-31 Shkval slew right",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+		},
+		["dnilp90u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "KU-31 Shkval slew up",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+		},
+		["dnilp91u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "KU-31 Shkval slew down",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+		},
+	},
+}
+return diff
