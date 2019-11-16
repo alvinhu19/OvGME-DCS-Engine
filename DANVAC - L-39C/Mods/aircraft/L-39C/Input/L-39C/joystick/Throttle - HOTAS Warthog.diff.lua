3c12717ccfcab7bcc323ea49866c1522a524ab34
Vaclav Danek <danvac@seznam.cz>
2019-11-16 09:36:17 +0100

    Ka-50: removed old assignments from diff files

---
+++ DANVAC - L-39C/Mods/aircraft/L-39C/Input/L-39C/joystick/Throttle - HOTAS Warthog.diff.lua
@@ -0,0 +1,113 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2012cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+			["name"] = "Zoom View",
+		},
+	},
+	["keyDiffs"] = {
+		["d1055pnilu1055cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN31",
+				},
+			},
+			["name"] = "Turbo Start Button",
+		},
+		["d147pnilunilcdnilvd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Airbrake Switch - OPEN",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+		},
+		["d148pnilunilcdnilvd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Airbrake Switch - CLOSE",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+		},
+		["d179pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Communication menu",
+		},
+		["d3099pnilunilcd4vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+				},
+			},
+			["name"] = "Engine Start Button Cover - OPEN/CLOSE",
+		},
+		["d3100pnilu3100cd4vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+			["name"] = "Engine Start Button",
+		},
+		["d3103pnilunilcd4vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+				},
+			},
+			["name"] = "Turbo Start Button Cover - OPEN/CLOSE",
+		},
+		["d313pnilu311cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle - Idle/Stop (hold)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN29",
+				},
+			},
+		},
+		["d430pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN22",
+				},
+			},
+			["name"] = "Landing Gear Handle, Up",
+		},
+		["d431pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Landing Gear Handle, Down",
+		},
+		["d523pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Flight - Attack ground targets",
+		},
+	},
+}
+return diff
